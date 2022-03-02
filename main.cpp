b05e8686416ffa804c1e4c4cdd5a41d1c85d3024
Malesi-Trevor-Git <trevormalesi4@gmail.com>
2022-03-02 18:01:41 +0300

    Commit

--- main.cpp
+++ main.cpp
@@ -17,53 +17,7 @@ static std::vector<int> twoSum(std::vector<int> &array,int target){
 
 };
 
-class Box{
-    private:
-    int l,b,h;
-    public:
-    Box(){
-        l=0;
-        b=0;
-        h=0;
-    }
-    Box(int length,int breadth,int height){
-        l=length;
-        b=breadth;
-        h=height;
-    }
-    Box(const Box &B){
-        l=B.l;
-        b=B.b;
-        h=B.h;
-    }
-    int getLength(){
-        return l;
-    }
-    int getBreadth(){
-        return b;
-    }
-    int getHeight(){
-        return h;
-    }
-    long long CalculateVolume(){
-        return l*h*b;
-    }
-    bool operator<(Box &B){
-        if (l<B.l){
-            return true;
-        }else if(l==B.l){
-            if (b<B.b){
-                return true;
-            }else if(b==B.b){
-                if (h<B.h){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
-};
 int main() {
     int j{};
     std::cin>>j;
